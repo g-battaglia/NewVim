bd6196939f1c189c70a237b36e825d395758439c
g-battaglia <battaglia.giacomo@icloud.com>
2026-03-30 15:18:10 +0200

    feat(neotree): enable follow_current_file to track active buffer

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -19,6 +19,7 @@ return {
 
     opts = {
       filesystem = {
+        follow_current_file = { enabled = true },
         filtered_items = {
           visible = true, 
           hide_dotfiles = false,
